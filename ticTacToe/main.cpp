58f52b349a5a3020a88a5e672ca654686a653c4d
Nick <burkettng@appstate.edu>
2020-11-11 12:20:32 -0500

    Initial Commit

---
+++ ticTacToe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ticTacToe
+//
+//  Created by Nick Burkett on 11/11/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
